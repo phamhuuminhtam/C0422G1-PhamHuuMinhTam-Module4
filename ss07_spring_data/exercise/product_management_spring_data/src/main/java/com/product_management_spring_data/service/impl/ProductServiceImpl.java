6e0992ef6ad03d586288cbc7d6c758d1cc961e89
phamhuuminhtam <minhtamk17qth@gmail.com>
2022-08-24 19:29:14 +0700

    up

--- ss07_spring_data/exercise/product_management_spring_data/src/main/java/com/product_management_spring_data/service/impl/ProductServiceImpl.java
+++ ss07_spring_data/exercise/product_management_spring_data/src/main/java/com/product_management_spring_data/service/impl/ProductServiceImpl.java
@@ -15,7 +15,6 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-
     @Override
     public Page<Product> findAllByName(String name, Pageable pageable) {
         return productRepository.findAllByNameContaining(name,pageable);
